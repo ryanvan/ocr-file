17efb82e75f4d5d46dcd5fbae209afed811b0078
carp <fanzhifeng@aboveseal.cn>
2024-03-08 18:08:45 +0800

    補充轉化語言支持

--- lib/ocr-file/ocr_engines/tesseract.rb
+++ lib/ocr-file/ocr_engines/tesseract.rb
@@ -8,12 +8,14 @@ module OcrFile
       end
 
       def ocr_to_text(file_path, options: {})
-        image = ::RTesseract.new(file_path)
+        options = { lang: options[:lang] } unless options[:lang].empty?
+        image = ::RTesseract.new(file_path, options)
         image.to_s # Getting the value
       end
 
       def ocr_to_pdf(file_path, options: {})
-        image = ::RTesseract.new(file_path)
+        options = { lang: options[:lang] } unless options[:lang].empty?
+        image = ::RTesseract.new(file_path, options)
         raw_output = image.to_pdf  # Getting open file of pdf
         OcrFile::ImageEngines::PdfEngine.open_pdf(raw_output, password: '')
       end
